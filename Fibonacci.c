189d2c21953d0616691ac88d9a42666f6158a6b3
srushtitibile7805-sys <srushtitibile7805@gmail.com>
2026-07-26 23:40:55 +0530

    Create Fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 0;
+    }
+
+    printf("Fibonacci Series: ");
+
+    for (int i = 1; i <= n; ++i) {
+        if (i == 1) {
+            printf("%d ", t1);
+            continue;
+        }
+        if (i == 2) {
+            printf("%d ", t2);
+            continue;
+        }
+        
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        
+        printf("%d ", nextTerm);
+    }
+    
+    printf("\n");
+
+    return 0;
+}
