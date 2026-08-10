bc69bcacace28f31da72702c1b27476984fe325a
srushtitibile7805-sys <srushtitibile7805@gmail.com>
2026-08-10 22:50:48 +0530

    Create LinearSwarch.c

---
+++ LinearSwarch.c
@@ -0,0 +1,21 @@
+#invlude<sdtio.c>
+int main()
+{
+  int n, i, key;
+printf("enter the no of element");
+scanf("%d",&n);
+int arr[n];
+printf("enter element:\n",n);
+for (i=0, i<n, i++)
+{
+ scanf("%d",&arr[i]);
+}
+ printf("enter the element to search:");
+scanf("%d",&key);
+for(i=0; i<0; i++)
+if(are[i]==key)
+{
+printf("element found at position:%d\n",i+1);
+return 0;
+}
+}
